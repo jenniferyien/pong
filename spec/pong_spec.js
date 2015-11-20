da513fd25500cee33b30d7c8abe674bd107c33c2
Jennifer Yien <jenniferllwu@gmail.com>
2015-11-20 09:29:04 -0500

    variable defined

--- spec/pong_spec.js
+++ spec/pong_spec.js
@@ -1,8 +1,17 @@
+  //testing by removing anonymous function
 describe("pong game", function(){
   describe("game size", function(){
     it("should have size of 700 by 600", function(){
-      expect($('canvas').width()).toEqual(700);
-      expect($('canvas').height()).toEqual(600);
+      expect(width).toEqual(700);
+      expect(height).toEqual(600);
     })
   });
+
+  describe("main play function", function(){
+    it("should be defined to play", function(){
+      expect(main()).toBe();
+    })
+  })
+
+  
 });
--- src/pong.js
+++ src/pong.js
@@ -1,8 +1,40 @@
-$(function onReady(){
+// $(function(){
   //declaring variables
   var width=700, height=600, pi=Math.PI;
   var canvas, ctx, keystate;
   var player, ai, ball;
+
+  player = {
+    x: null,
+    y: null,
+    width: 20,
+    height: 100,
+    update: function() {},
+    draw: function(){
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  };
+
+  ai = {
+    x: null,
+    y: null,
+    width: 20,
+    height: 100,
+    update: function() {},
+    draw: function(){
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  };
+
+  ball(
+    x: null,
+    y: null,
+    side: 20,
+    update: function() {},
+    draw: function(){
+      ctx.fillRect(this.x, this.y, this.side, this.side);
+    }
+  );
 
   function main(){
     canvas = document.createElement("canvas");
@@ -10,7 +42,18 @@ $(function onReady(){
     canvas.height = height;
     ctx = canvas.getContext("2d");
     document.body.appendChild(canvas)
+
+    init();
+
+    var loop = function() {
+      update();
+      draw();
+
+      window.requestAnimationFrame(loop, canvas);
+    };
+    window.requestAnimationFrame(loop, canvas);
   }
+
   function init(){}
 
   function update(){}
@@ -18,4 +61,4 @@ $(function onReady(){
   function draw(){}
 
   main();
-});
+// });
