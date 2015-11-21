a5fe3dade3318cb3261a4011f54b70ba56f0010a
Jennifer Yien <jenniferllwu@gmail.com>
2015-11-21 10:20:53 -0500

    init() set up

--- spec/pong_spec.js
+++ spec/pong_spec.js
@@ -4,14 +4,20 @@ describe("pong game", function(){
     it("should have size of 700 by 600", function(){
       expect(width).toEqual(700);
       expect(height).toEqual(600);
-    })
+    });
   });
 
   describe("main play function", function(){
     it("should be defined to play", function(){
       expect(main()).toBe();
-    })
-  })
+    });
+  });
 
-  
+  describe("variables defined", function(){
+    it("should have methods for the player, ai, and ball", function(){
+      expect(typeof player).toEqual('object');
+      expect(typeof ai).toBe('object');
+      expect(typeof ball).toEqual('object');
+    });
+  });
 });
--- src/pong.js
+++ src/pong.js
@@ -4,6 +4,7 @@
   var canvas, ctx, keystate;
   var player, ai, ball;
 
+  //setting up player objects
   player = {
     x: null,
     y: null,
@@ -26,7 +27,7 @@
     }
   };
 
-  ball(
+  ball = {
     x: null,
     y: null,
     side: 20,
@@ -34,17 +35,21 @@
     draw: function(){
       ctx.fillRect(this.x, this.y, this.side, this.side);
     }
-  );
+  };
 
+  //main play function
   function main(){
+    //setting up html canvas
     canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
     ctx = canvas.getContext("2d");
     document.body.appendChild(canvas)
 
+    //call the initize play function
     init();
 
+    //loop to update status of game
     var loop = function() {
       update();
       draw();
@@ -54,7 +59,17 @@
     window.requestAnimationFrame(loop, canvas);
   }
 
-  function init(){}
+  //setting up the x y position of player ai and ball
+  function init(){
+    player.x = player.width;
+    player.y = (height - player.height)/2;
+
+    ai.x = width - (player.width + ai.width);
+    ai.y = (height - ai.height)/2;
+
+    ball.x = (width - ball.side)/2;
+    ball.y = (height - ball.side)/2;
+  }
 
   function update(){}
 
